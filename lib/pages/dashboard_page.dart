8ec7f0414aca76a51eb5b7287b5a4970ee97e212
shadabhashmi21 <er.shadabhashmi@gmail.com>
2021-12-06 13:28:38 +0530

    finalized bottom navigation bar

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -9,20 +9,52 @@ class DashboardPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final _selectedIndex = useState(0);
+    final _selectedHeartColor = useState(Colors.red);
+    final _selectedForegroundColor = useState(Colors.white);
+    final _selectedBackgroundColor = useState(Colors.blue);
+    final _unselectedBackgroundColor = useState(Colors.white);
+    final _unselectedForegroundColor = useState(Colors.black);
+
+    Color _getBgColor(int index) =>
+        _selectedIndex.value == index ? _selectedBackgroundColor.value : _unselectedBackgroundColor.value;
+
+    Color _getItemColor(int index) =>
+        _selectedIndex.value == index ? _selectedForegroundColor.value : _unselectedForegroundColor.value;
+
+    Widget _buildIcon(IconData iconData, String text, int index) => SizedBox(
+      width: double.infinity,
+      height: kBottomNavigationBarHeight,
+      child: Material(
+        color: _getBgColor(index),
+        child: InkWell(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(iconData,color: index == 1 ? _selectedHeartColor.value :_getItemColor(index),),
+              const SizedBox(width: 10,),
+              Text(text,
+                  style: TextStyle(color: _getItemColor(index))),
+            ],
+          ),
+        ),
+      ),
+    );
 
     return Scaffold(
       body: _selectedIndex.value == 0
           ? const NewsListPage()
           : const FavoritesListPage(),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        items:  [
           BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted),
-            label: 'News',
+            icon: _buildIcon(Icons.format_list_bulleted, 'News', 0),
+              label: ''
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favs',
+            icon: _buildIcon(Icons.favorite, 'Favs', 1),
+            label: ''
           ),
         ],
         selectedItemColor: Colors.blue,
